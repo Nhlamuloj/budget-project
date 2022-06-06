ae74353dd4fb1dfd89d298e79106591f43f3e487
nhlamuloj <nmahlabana@gmail.com>
2022-06-06 15:53:48 +0200

    first commit

---
+++ src/components/additem.js
@@ -0,0 +1,21 @@
+function AddItem(){
+    return(
+        <div>
+            <input placeholder="Enter item"/>
+            <input placeholder="Enter item"/>
+
+            <select>
+                <option value="Income">Income</option>
+                <option value="Income">Expense</option>
+            </select>
+            <button>Add</button>
+
+        </div>
+
+        
+    )
+}
+
+
+
+export default AddItem
