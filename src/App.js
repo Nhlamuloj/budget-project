fa23fb62f72aa341c8862cf69ee8e605347a0cab
nhlamuloj <nmahlabana@gmail.com>
2022-06-07 16:13:34 +0200

    first commit

--- src/App.js
+++ src/App.js
@@ -1,12 +1,27 @@
 import logo from './logo.svg';
 import './App.css';
 
+import React,{useState} from 'react';
+
 import AddItem from './components/additem';
 
 function App() {
+
+  const [transaction, setTransaction] = useState ([]);
+  const addTransaction = ((amout, item, transactionType)=>{
+
+    setTransaction ((item)=>[...item,{
+      amount:amout,
+      item:item,
+      transactionType:transactionType,
+    }])
+  })
+
   return (
     <div className="container">
-      <AddItem/>  
+
+      <AddItem add={ addTransaction} />  
+
     </div>
   );
 }
--- src/components/additem.js
+++ src/components/additem.js
@@ -1,16 +1,36 @@
-function AddItem(){
+import React, {useState} from "react";
+import "../css/add.css"
+function AddItem(props){
+
+    const [amougitt, setAmount] = useState("");
+    const [item, setItem] = useState("");
+    const [transactionType, setTransactionType] = useState("");
+
+
+
+
+    const add = (()=>{
+        console.log(item);
+        console.log(amout)
+        console.log(transactionType)
+        props.add (amout, item, transactionType);
+    })
     return(
-        <div>
-            <input placeholder="Enter item"/>
-            <input placeholder="Enter item"/>
+     <div>
+         <input placeholder="Enter item"
+          onChange={(e)=>setItem(e.target.value)}
+         /><br></br>
+         <input placeholder="Enter amout"
+         onChange={(e)=>setAmount(e.target.value)}
+         /><br></br>
+         <select  ><br></br>
+             <option value="Income">Income</option><br></br>
+             <option value="Expense">Expense</option><br></br>
+         </select><br></br>
+         <button id="btn" onClick={add}>Add</button>
+     </div>  
 
-            <select>
-                <option value="Income">Income</option>
-                <option value="Income">Expense</option>
-            </select>
-            <button>Add</button>
-
-        </div>
+    
 
         
     )
